aa688ca82cc4a53f3b4408469245fabf78442b1c
Henry <basedhenry@berkeley.edu>
2016-09-20 21:45:45 -0700

    hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,16 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    b = Array.new
+    sum = 0
+    for c in a
+    	c_i = c.to_i + 2
+    	if c_i % 2 == 0 && c_i <= 10 && !b.include?(c_i)
+			b.push(c_i)
+			sum += c_i	
+    	end
+    end
+    return sum
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,20 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  return (a+b)**2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  b = Array.new
+  sortedA=a.sort
+  sortedA.each {|x| b.push(x+1)}
+  return b
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
